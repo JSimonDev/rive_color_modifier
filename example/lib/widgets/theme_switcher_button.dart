899b889bdca7e9e647cd5056337ceb4c6e6f0748
Jonathan Simon <yuditbertoli@gmail.com>
2024-08-27 15:12:37 -0400

    widget to change theme of the app

---
+++ example/lib/widgets/theme_switcher_button.dart
@@ -0,0 +1,34 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:example/config/config.dart';
+import 'package:flutter/material.dart';
+
+/// A button that allows the user to switch between light and dark themes.
+class ThemeSwitcherButton extends StatelessWidget {
+  const ThemeSwitcherButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    final appThemeDark = AppTheme(isDarkMode: true).getTheme();
+    final appThemeLight = AppTheme(isDarkMode: false).getTheme();
+
+    return ThemeSwitcher.switcher(
+      builder: (context, switcher) {
+        return IconButton(
+          iconSize: 50,
+          icon: Icon(
+            isDarkMode ? Icons.wb_sunny_rounded : Icons.nights_stay_rounded,
+          ),
+          onPressed: () async {
+            switcher.changeTheme(
+              isReversed: isDarkMode ? true : false,
+              theme: isDarkMode ? appThemeLight : appThemeDark,
+            );
+          },
+        );
+      },
+    );
+  }
+}
