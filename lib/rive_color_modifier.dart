d0c658d4e98a89eae5070763675da863ded16065
Jonathan Simon <jsimondev@gmail.com>
2024-12-17 03:38:13 -0400

    fix: clean up exports in rive_color_modifier.dart for better module structure

--- lib/rive_color_modifier.dart
+++ lib/rive_color_modifier.dart
@@ -1,3 +1,7 @@
+export 'src/rive_color_component.dart';
+export 'src/rive_color_modifier.dart';
+export 'src/rive_gradient_component.dart';
+
 ///  Author: Jonathan Simon
 ///  Website: jsimon.dev
 
@@ -7,9 +11,3 @@
 ///This package provides an easy and straightforward way to modify the Color
 ///of specific Components within Rive Animations at runtime, while also
 ///maintaining their opacity (alpha values) throughout the animation.
-
-library rive_color_modifier;
-
-export 'src/rive_color_component.dart';
-export 'src/rive_gradient_component.dart';
-export 'src/rive_color_modifier.dart';
