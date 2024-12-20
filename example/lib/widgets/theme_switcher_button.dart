1b4dab3fad648ca6a500a308d05b8376f72c88ed
Jonathan Simon <jsimondev@gmail.com>
2024-12-20 19:05:30 -0400

    feat: add haptic feedback to theme switcher button

--- example/lib/widgets/theme_switcher_button.dart
+++ example/lib/widgets/theme_switcher_button.dart
@@ -1,6 +1,7 @@
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:example/config/config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 /// A button that allows the user to switch between light and dark themes.
 class ThemeSwitcherButton extends StatelessWidget {
@@ -22,6 +23,7 @@ class ThemeSwitcherButton extends StatelessWidget {
             isDarkMode ? Icons.wb_sunny_rounded : Icons.nights_stay_rounded,
           ),
           onPressed: () async {
+            HapticFeedback.lightImpact();
             switcher.changeTheme(
               isReversed: isDarkMode ? true : false,
               theme: isDarkMode ? appThemeLight : appThemeDark,
